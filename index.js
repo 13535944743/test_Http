6625701380fb6e530f47e3a27d1aef83a67db02c
clz <1926389897@qq.com>
2023-04-07 20:19:26 +0800

    clz: test

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var url = require('url');
 var https = require('https');
-var { SocksProxyAgent } = require('socks-proxy-agent');
+// var { SocksProxyAgent } = require('socks-proxy-agent');
 
 const fs = require('fs');
-const { stdin } = require('process');
 
 // SOCKS proxy to connect to
-var proxy = 'socks://127.0.0.1:10808';
+// var proxy = 'socks://127.0.0.1:10808';
 
 // HTTP endpoint for the proxy to connect to
-var endpoint = 'https://www.google.com';
-var opts = url.parse(endpoint);
+// var endpoint = 'https://www.google.com';
+// var opts = url.parse(endpoint);
 
 // create an instance of the `SocksProxyAgent` class with the proxy server information
 var agent = new SocksProxyAgent(proxy);
