83c0a073ab25fe275ee4b8bbd48f0cee8cf16190
赤蓝紫 <71619858+13535944743@users.noreply.github.com>
2023-04-07 09:19:19 +0800

    test

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const options = {
+  hostname: 'www.google.com',
+  path: '/',
+  method: 'GET',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+  },
+  timeout: 60000
+};
+
+const req = http.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', (error) => {
+  console.error(error);
+});
+
+req.end();
